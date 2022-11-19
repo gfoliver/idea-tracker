949004aff395ecd83506f02a98752684fbafd00b
Guilherme Fleck Oliveira <guilhermefleckoliveira@gmail.com>
2022-11-19 17:41:17 -0300

    Responsive fix

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -10,14 +10,14 @@ const Dashboard: React.FC = () => {
         <div className="Dashboard min-h-screen flex flex-col">
             <Modal ref={modalRef} />
             <header>
-                <div className="container mx-auto py-12">
-                    <div className="flex justify-between items-center">
-                        <h1 className="text-5xl font-bold flex items-center">
+                <div className="container mx-auto py-12 px-4">
+                    <div className="block md:flex justify-between items-center">
+                        <h1 className="text-2xl lg:text-5xl font-bold flex items-center mb-8 md:mb-0">
                             <img src={icon} alt="Light Bulb" className='mr-2' />
                             IdeaTracker
                         </h1>
                         <button 
-                            className='bg-background-lighter py-5 px-12 rounded-lg drop-shadow-md text-primary font-bold text-2xl'
+                            className='bg-background-lighter py-4 px-8 lg:py-5 lg:px-12 rounded-lg drop-shadow-md text-primary font-bold text-lg lg:text-2xl w-full md:w-auto'
                             onClick={() => modalRef.current?.open()}
                         >
                             I've had an idea!
@@ -26,8 +26,8 @@ const Dashboard: React.FC = () => {
                 </div>
             </header>
             <main className='bg-background-darker flex-grow'>
-                <div className="container mx-auto py-14">
-                    <div className="grid grid-cols-4 gap-4">
+                <div className="container mx-auto py-14 px-4">
+                    <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
                         {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((idea) => (
                             <div className="idea p-6 bg-background rounded-lg drop-shadow-sm">
                                 <h2 className="font-bold mb-4">Idea {idea}</h2>
--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -20,7 +20,7 @@ const Modal = React.forwardRef<IModal>((props, ref) => {
         >
             <Dialog.Overlay className="fixed inset-0 flex items-center justify-center">
                 <Dialog.Backdrop className="fixed inset-0 bg-black opacity-60" />
-                <Dialog.Panel className="bg-background px-8 p-10 max-w-full rounded-lg" style={{minWidth: 560}}>
+                <Dialog.Panel className="bg-background px-8 p-10 w-full min-h-screen md:max-w-xl md:min-h-0 md:rounded-lg">
                     <h2 className="text-4xl text-white font-bold mb-8">New Idea</h2>
                     <div className="mb-4 w-full">
                         <label htmlFor="new-idea-name" className='block text-white font-bold mb-2'>Name</label>
@@ -31,6 +31,7 @@ const Modal = React.forwardRef<IModal>((props, ref) => {
                         <textarea id="new-idea-description" className='w-full p-4 rounded bg-background-darker text-white resize-y' />
                     </div>
                     <button className='bg-primary p-4 w-full rounded font-bold text-white' onClick={() => setIsOpen(false)}>Save</button>
+                    <button className='bg-background mt-6 w-full rounded font-bold text-white' onClick={() => setIsOpen(false)}>Close</button>
                 </Dialog.Panel>
             </Dialog.Overlay>
         </Dialog>
